338f469d33d0b1ac533b1544ad0e11576d3db58b
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-19 11:28:31 +0200

    wip

---
+++ src/main/java/com/example/testspringjava/TestSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.testspringjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testspringjava/TestSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspringjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
