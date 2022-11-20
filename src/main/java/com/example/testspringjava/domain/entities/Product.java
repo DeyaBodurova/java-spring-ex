d4552ef4f5bcc1c4f97bf3492ca94cb76ddb1298
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-20 13:40:55 +0200

    wip

--- src/main/java/com/example/testspringjava/domain/entities/Product.java
+++ src/main/java/com/example/testspringjava/domain/entities/Product.java
@@ -1,4 +1,41 @@
 package com.example.testspringjava.domain.entities;
 
-public class Product {
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import javax.persistence.Column;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+public class Product extends BaseEntity {
+
+    @Column
+    private String name;
+
+    @Column
+    private Double price;
+
+    @ManyToOne
+    @Fetch(FetchMode.JOIN)
+    private User buyer;
+
+    @ManyToOne
+    @Fetch(FetchMode.JOIN)
+    private User seller;
+
+    @ManyToMany
+    @Fetch(FetchMode.JOIN)
+    private Set<Category> categories;
 }
