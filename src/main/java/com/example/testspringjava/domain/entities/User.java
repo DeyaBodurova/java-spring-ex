c8543ae7299e0448ddcf25e612f7cdf686b4d6a8
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-20 18:02:05 +0200

    wip

--- src/main/java/com/example/testspringjava/domain/entities/User.java
+++ src/main/java/com/example/testspringjava/domain/entities/User.java
@@ -35,7 +35,6 @@ public class User extends BaseEntity {
     @Fetch(FetchMode.JOIN)
     private Set<Product> boughtProducts;
 
-
     @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE})
     @Fetch(FetchMode.JOIN)
     private Set<User> friends;
