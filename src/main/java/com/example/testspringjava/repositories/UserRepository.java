961aa76dcdb1c56e67cb72fc57a121c16e3d4f67
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-21 07:15:23 +0200

    wip

--- src/main/java/com/example/testspringjava/repositories/UserRepository.java
+++ src/main/java/com/example/testspringjava/repositories/UserRepository.java
@@ -2,8 +2,14 @@ package com.example.testspringjava.repositories;
 
 import com.example.testspringjava.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query(value = "select * from `test-spring`.users order by RAND () LIMIT 1",
+            nativeQuery = true)
+    Optional<User> getRandomEntity();
 }
